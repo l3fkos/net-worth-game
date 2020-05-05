d460394cfc52c26c213ed492cd1eb891dce80c1a
lefkos <balexny@gmail.com>
2020-05-04 23:27:37 -0400

    inital commit

---
+++ database/migrations/2020_05_05_003453_create_celebrities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCelebritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('celebrities', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->bigInteger('net_worth');
+            $table->string('category')->nullable();
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('celebrities');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-12 jumbotron mt-4 text-center">
+            <div>
+                <h1>Who's Richer?</h1>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="jumbotron mt-2 col-md-5 text-center">
+            <div>
+                <h1>Conor McGregor</h1>
+            </div>
+        </div>
+        <div class="jumbotron mt-2 col-md-5 text-center ml-2">
+            <div>
+                <h1>Nicolas Cage</h1>
+            </div>
+        </div>
+    </div>
+@endsection
